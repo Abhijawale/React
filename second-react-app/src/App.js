8baa49c7474c4058f2b94bebba1ed3b5b8d6d73b
66643_Abhijeet <abhijawale12@gmail.com>
2023-06-13 22:20:54 +0530

    second commit

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,14 +1,12 @@
+// JSX with 1 Tag or 1 H1
+// JSX with 2 Tag or 2 H1
+// RULE::IF USING MORE THAN 1 TAG; IT MUST BE UUNDER PARENT
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
 }
-
-
-
-
-// UNUSED!!!
-// JSX => JS + HTML/XML
-// IF your function returns HTML
-// Convention:: Start your function name in captial letter.
-
 
 export default App;
