b42ba81e88b657273b874dacdbfb6e18fabaed80
66643_Abhijeet <abhijawale12@gmail.com>
2023-06-13 22:18:18 +0530

    first commit

---
+++ second-react-app/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return <h1>Hello</h1>;
+}
+
+
+
+
+// UNUSED!!!
+// JSX => JS + HTML/XML
+// IF your function returns HTML
+// Convention:: Start your function name in captial letter.
+
+
+export default App;
